02d76709a95cb162d8e43b81e8f5bf61185ee496
Chip Franzen <chip.franzen@gmail.com>
2025-06-08 17:12:44 -0700

    Add home-manager as a package.

--- home/home.nix
+++ home/home.nix
@@ -22,6 +22,7 @@
     # # "Hello, world!" when run.
     # pkgs.hello
     pkgs.zsh
+    pkgs.home-manager
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
